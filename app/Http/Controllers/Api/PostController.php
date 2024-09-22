f01fe57b389ec9335c077c2bad4c0c11cdd780ce
UltimateDestruct <ultimatedestruct@gmail.com>
2024-09-21 20:11:52 -0500

    Added Categories and Table Sorting

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -6,12 +6,28 @@ use App\Models\Post;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\PostResource;
+use Illuminate\Database\Eloquent\Builder;
 
 class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::with('category')->paginate(10);
+        $orderColumn = request('order_column', 'created_at');
+        if(! in_array($orderColumn, ['id', 'title', 'created_at'])) {
+            $orderColumn = 'createde_at';   
+        }
+
+        $orderDirection = request('order_direction', 'desc');
+        if(! in_array($orderDirection, ['asc', 'desc']) ) {
+            $orderDirection = 'desc';
+        }
+
+        $posts = Post::with('category')
+        ->when(request('category'), function (Builder $query) {
+            $query->where('category_id', request('category'));
+        })
+        ->orderBy($orderColumn, $orderDirection)
+        ->paginate(10);
 
         return PostResource::collection($posts);
     }
--- routes/api.php
+++ routes/api.php
@@ -3,9 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\PostController;
+use App\Http\Controllers\Api\CategoryController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
 Route::get('posts', [PostController::class, 'index']);
+Route::get('categories', [CategoryController::class, 'index']);

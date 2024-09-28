4e64115bc89a15b6ddcdb5e9db44a1c0e929b857
UltimateDestruct <ultimatedestruct@gmail.com>
2024-09-27 19:34:08 -0500

    Vue Routing

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/', 'dashboard')->name('dashboard'); 
+Route::view('/{any?}', 'dashboard')->name('dashboard')
+    ->where('any', '.*'); 
 // Route::get('/', function () {
 //     return view('welcome');
 // });

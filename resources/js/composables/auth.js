0ff08af1e81072c6c7d176293cb84e822d3533dc
Cody Hanson <ultimatedestruct@gmail.com>
2025-08-16 20:51:48 -0500

    Hide UI elements if user doesn't have permission

--- resources/js/composables/auth.js
+++ resources/js/composables/auth.js
@@ -1,5 +1,7 @@
 import { ref, reactive, inject } from 'vue'
 import { useRouter } from 'vue-router'
+import { AbilityBuilder, Ability } from '@casl/ability'
+import { ABILITY_TOKEN } from '@casl/vue'
 
 const user = reactive({
     name: '',
@@ -11,6 +13,7 @@ export default function useAuth() {
     const validationErrors = ref({})
     const router = useRouter()
     const swal = inject('$swal')
+    const ability = inject(ABILITY_TOKEN)
     const loginForm = reactive({
         email: '',
         password: '',
@@ -35,12 +38,13 @@ export default function useAuth() {
             .finally(() => processing.value = false)
     }
 
-    const loginUser = (response) => {
+    const loginUser = async (response) => {
         user.name = response.data.name
         user.email = response.data.email
 
         localStorage.setItem('loggedIn', JSON.stringify(true))
-        router.push({ name: 'posts.index'})
+        await getAbilities()
+        await router.push({ name: 'posts.index'})
     }
 
     const logout = async () => {
@@ -69,6 +73,16 @@ export default function useAuth() {
             })
     }
 
+    const getAbilities = async() => {
+        axios.get('/api/abilities')
+            .then(response => {
+                const permissions = response.data
+                const { can, rules} = new AbilityBuilder(Ability)
+                can(permissions)
+                ability.update(rules)
+            })
+    }
+
     return { 
         loginForm, 
         validationErrors, 
@@ -77,5 +91,6 @@ export default function useAuth() {
         user, 
         getUser,
         logout,
+        getAbilities,
     }
 }
